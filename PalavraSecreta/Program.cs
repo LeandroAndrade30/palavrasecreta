0085f9e4ca2375390e4e279a66446af969e96af9
Leandro Andrade <leandrade666@gmail.com>
2024-02-27 13:28:12 -0300

    Criado Progeto Palavra Secreta

---
+++ PalavraSecreta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalavraSecreta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
