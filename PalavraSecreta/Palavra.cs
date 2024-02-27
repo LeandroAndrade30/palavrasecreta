f05f6ae32d1f9ce2e4ecd4715d8a529ed061920c
Leandro Andrade <leandrade666@gmail.com>
2024-02-27 15:22:44 -0300

    Criada versão do Programa Palavra Secreta com Orientação a Objetos

---
+++ PalavraSecreta/Palavra.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalavraSecreta
+{
+    class Palavra
+    {
+        public string PalavraSecreta;
+        public string Descricao;
+
+        public void AdivinheAPalavraSecreta()
+
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("\nTentativa " + (i + 1) + " Informe a palavra  secreta:");
+                string tentativa = (Console.ReadLine().ToLower());
+
+                if (tentativa == PalavraSecreta.ToLower())
+                {
+                    Console.WriteLine("Parabéns, Você acertou a palavra secreta");
+                    break;
+                }
+                else if (i == 4)
+                {
+                    Console.WriteLine("Suas tentativas terminaram, o jogo acabou!");
+                }
+                else
+                {
+                    Console.WriteLine("Você errou, tente novamente!");
+                }
+
+            }
+
+        }
+    }
+}
+
+        
--- PalavraSecreta/Program.cs
+++ PalavraSecreta/Program.cs
@@ -6,34 +6,20 @@ namespace PalavraSecreta
     {
         static void Main(string[] args)
         {
+            Palavra Palavra = new Palavra();
+
             Console.WriteLine("Informe a palavra secreta: ");
-            string palavraSecreta = Console.ReadLine();
+            Palavra.PalavraSecreta = Console.ReadLine();
             Console.WriteLine("Informe a descricção da palavra secreta: ");
-            string descrição = Console.ReadLine();
+            Palavra.Descricao = Console.ReadLine();
             Console.Clear();
-            Console.WriteLine($"A descrição da palavra secreta é: {descrição }");
+            Console.WriteLine($"A descrição da palavra secreta é: {Palavra.Descricao }");
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("\nTentativa " +   (i+1) + " Informe a palavra  secreta:");
-                string tentativa = (Console.ReadLine().ToLower());
 
-                if (tentativa == palavraSecreta)
-                {
-                    Console.WriteLine("Parabéns, Você acertou a palavra secreta");
-                    break;
-                }
-                else if (i == 4)
-                {
-                    Console.WriteLine("Suas tentativas terminaram, o jogo acabou!");
-                }
-                else
-                {
-                    Console.WriteLine("Você errou, tente novamente!");
-                }
 
-            }
 
-        } 
+            Palavra.AdivinheAPalavraSecreta();
+        }
+
     }
 }
