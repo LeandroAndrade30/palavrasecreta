608e92b4f0af9dc43ee4b2cac879f6a96e9c1a7f
Leandro Andrade <leandrade666@gmail.com>
2024-02-27 14:32:12 -0300

    Criada versão do Projeto Palavra secreta sem Orientação a objetos

--- PalavraSecreta/Program.cs
+++ PalavraSecreta/Program.cs
@@ -6,7 +6,34 @@ namespace PalavraSecreta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Informe a palavra secreta: ");
+            string palavraSecreta = Console.ReadLine();
+            Console.WriteLine("Informe a descricção da palavra secreta: ");
+            string descrição = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"A descrição da palavra secreta é: {descrição }");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("\nTentativa " +   (i+1) + " Informe a palavra  secreta:");
+                string tentativa = (Console.ReadLine().ToLower());
+
+                if (tentativa == palavraSecreta)
+                {
+                    Console.WriteLine("Parabéns, Você acertou a palavra secreta");
+                    break;
+                }
+                else if (i == 4)
+                {
+                    Console.WriteLine("Suas tentativas terminaram, o jogo acabou!");
+                }
+                else
+                {
+                    Console.WriteLine("Você errou, tente novamente!");
+                }
+
+            }
+
+        } 
     }
 }
